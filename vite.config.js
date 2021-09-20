b9b2dd21979b077b5f0611bcafbee254f2eaf620
Brama Udi <bramaudi.al@gmail.com>
2021-09-20 19:51:55 +0700

    chore: paths alias in TSConfig

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
-import Pages from "vite-plugin-pages";
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite"
+import solidPlugin from "vite-plugin-solid"
+import tsconfigPaths from "vite-tsconfig-paths"
+import Pages from "vite-plugin-pages"
+import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     VitePWA({
       workbox: {
         globPatterns: [
