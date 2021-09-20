bbec8abd035ab721d42f5f9b3752747f47abe21e
Brama Udi <bramaudi.al@gmail.com>
2021-09-21 05:49:02 +0700

    chore: move PWA config

--- vite.config.js
+++ vite.config.js
@@ -3,47 +3,15 @@ import solidPlugin from "vite-plugin-solid"
 import tsconfigPaths from "vite-tsconfig-paths"
 import Pages from "vite-plugin-pages"
 import { VitePWA } from "vite-plugin-pwa"
+import PWAConfig from "./src/config/pwa"
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    VitePWA({
-      workbox: {
-        globPatterns: [
-          '**/*.{js,css,html,svg,png,jpg}',
-        ]
-      },
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Solid App',
-        short_name: 'Solid App',
-        description: 'Solid + Vite + Tailwind',
-        icons: [
-          {
-            "src": "/android-chrome-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
-          },
-          {
-            "src": "/android-chrome-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          }
-        ],
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-      }
-    }),
+    VitePWA(PWAConfig),
     Pages({
       react: true,
-      importMode: 'async'
+      importMode: 'async',
     }),
     solidPlugin(),
   ],
